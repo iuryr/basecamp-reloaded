58f28336a768976adb52828ccac800121db1b295
Iury Santos <iusantos@c1r8p2.42sp.org.br>
2023-04-19 13:52:49 -0300

    ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 13:36:29 by iusantos          #+#    #+#             */
+/*   Updated: 2023/04/19 13:38:37 by iusantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = 48;
+	while (i < 58)
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
