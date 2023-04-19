42b21ff150b893f120a9dc3dad60ff723fcbcec5
Iury Santos <iusantos@c1r8p2.42sp.org.br>
2023-04-19 14:01:02 -0300

    ex08

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 13:59:50 by iusantos          #+#    #+#             */
+/*   Updated: 2023/04/19 14:00:22 by iusantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
