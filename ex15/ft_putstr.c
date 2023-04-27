974958c14ee05cf56237aa32d15fe6e9efc309fa
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-04-27 18:26:03 -0300

    adequando à norma

--- ex15/ft_putstr.c
+++ ex15/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 13:21:51 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/20 13:31:24 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/27 18:25:43 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,9 @@ void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		ft_putchar(*str);
-		str++;	
+		str++;
 	}
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
 }
