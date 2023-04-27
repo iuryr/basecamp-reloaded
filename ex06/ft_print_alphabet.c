0c164ce89d742b0465af037eb1276d7351226ba6
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-04-27 18:17:21 -0300

    tirando declaração da ft_putchar

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:48:40 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/19 12:48:48 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/27 18:16:59 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,4 @@ void	ft_print_alphabet(void)
 		ft_putchar(i);
 		i++;
 	}
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
 }
