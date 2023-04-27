a81914ba620d90a8917f2ec548601046658b1709
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-04-27 18:28:13 -0300

    retirando declaracao da ft_putchar

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -6,16 +6,13 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 14:13:54 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/20 14:28:43 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/27 18:27:42 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
