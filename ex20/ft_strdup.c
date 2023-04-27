4671ce715b55edd1cab33cfc0312d4038499f057
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-04-27 18:44:20 -0300

    adequacao a norma

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 18:42:26 by iusantos          #+#    #+#             */
+/*   Updated: 2023/04/27 18:44:05 by iusantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-char *ft_strdup(char *src);
+char	*ft_strdup(char *src);
 
 int	ft_strlen(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
 	return (i);
 }
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-	char *dest;
-	int	size;
-	int	i;
+	char	*dest;
+	int		size;
+	int		i;
 
 	i = 0;
 	size = ft_strlen(src) + 1;
