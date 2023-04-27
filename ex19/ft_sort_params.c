458d7ca3012c2dfb8f0c1c6845341f522541a70e
Iury Santos <iusantos@c1r7p2.42sp.org.br>
2023-04-27 18:41:27 -0300

    adequando à norma

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -1,15 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 18:30:02 by iusantos          #+#    #+#             */
+/*   Updated: 2023/04/27 18:41:06 by iusantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putstr(char *str);
 void	ft_print_params(int argc, char *argv[]);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
 void	ft_swap(char **param1, char **param2);
 void	ft_sort_params(int argc, char *argv[]);
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
@@ -20,27 +28,14 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	ft_print_params(int argc, char *argv[])
-{
-	int i;
-
-	i = 1;
-	while (i < argc)
-	{
-		ft_putstr(argv[i]);
-		ft_putchar('\n');
-		i++;
-	}
-}
-
 int	ft_strcmp(char *s1, char *s2)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if  (s1[i] != s2[i])
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
@@ -49,7 +44,7 @@ int	ft_strcmp(char *s1, char *s2)
 
 void	ft_swap(char **p1_addr, char **p2_addr)
 {
-	char *tmp;
+	char	*tmp;
 
 	tmp = *p1_addr;
 	*p1_addr = *p2_addr;
@@ -58,8 +53,8 @@ void	ft_swap(char **p1_addr, char **p2_addr)
 
 void	ft_sort_params(int argc, char *argv[])
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 1;
 	while (i < argc)
@@ -70,7 +65,6 @@ void	ft_sort_params(int argc, char *argv[])
 			if (ft_strcmp(argv[i], argv[j]) > 0)
 				ft_swap(&argv[i], &argv[j]);
 			j++;
-
 		}
 		i++;
 	}
@@ -78,13 +72,20 @@ void	ft_sort_params(int argc, char *argv[])
 
 int	main(int argc, char *argv[])
 {
+	int	i;
+
 	if (argc < 2)
 		return (0);
 	else
 	{
 		ft_sort_params(argc, argv);
-		ft_print_params(argc, argv);
-
+		i = 1;
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
 	}
 	return (1);
 }
