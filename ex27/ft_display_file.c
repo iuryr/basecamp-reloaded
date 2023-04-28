b2a63382cf1a2233f2c38430a07c1ed976d956c4
Iury Santos <iusantos@c1r5p4.42sp.org.br>
2023-04-28 14:55:16 -0300

    explicitando tamanho 1 para buffer

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:53:57 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/28 14:21:07 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:54:11 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	main(int argc, char *argv[])
 {
 	int		fd;
-	char	buf;
+	char	buf[1];
 
 	if (argc == 1)
 	{
