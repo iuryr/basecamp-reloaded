b8715250b0309988fc18df83de6600e58f6d6004
Iury Santos <iusantos@c1r5p4.42sp.org.br>
2023-04-28 14:02:27 -0300

    trocando fd 2 por 1 nos writes

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:53:57 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/28 13:35:06 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:01:12 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,20 @@ int	main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		write(2, "File name missing.\n", 20);
+		write(1, "File name missing.\n", 20);
 		return (1);
 	}
 	if (argc > 2)
 	{
-		write(2, "Too many arguments.\n", 21);
+		write(1, "Too many arguments.\n", 21);
 		return (1);
 	}
 	else
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-			return (1);
 		while (read(fd, &buf, 1))
 			write(1, &buf, 1);
-		if (close(fd) == -1)
-			return (1);
+		close(fd);
 		return (0);
 	}
 }
