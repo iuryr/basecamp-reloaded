3c1a7424f5da08bab46bdfad2ed719d58c76d8e9
Iury Santos <iusantos@c1r5p4.42sp.org.br>
2023-04-28 12:29:37 -0300

    limitando tamanho no input

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 10:54:10 by iusantos          #+#    #+#             */
-/*   Updated: 2023/04/20 11:27:01 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/04/28 12:25:24 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 19)
 		return (0);
 	if (nb == 0)
 		return (1);
